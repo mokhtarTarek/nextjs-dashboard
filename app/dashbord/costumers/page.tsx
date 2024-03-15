f19af7e1c3e3b31da70c33880a48223431ad3fa2
mokhtarTarek <tmkh@outlook.fr>
2024-03-15 11:17:26 +0100

    Dashbord next js project

---
+++ app/dashbord/costumers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+    return (
+        <p>costumers</p>
+    )
+}
+
+export default Page
---
+++ app/dashbord/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+    return (
+        <p>invoice page</p>
+    )
+}
+
+export default Page
